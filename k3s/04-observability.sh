67be89a4b47538ea327bbfaa746a5bcf016f72f0
Kevin Jayne <kevin.jayne@injectivelabs.org>
2025-12-31 01:55:51 -0500

    redeploy-grafana

--- k3s/04-observability.sh
+++ k3s/04-observability.sh
@@ -7,6 +7,8 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
+kubectl create namespace observability
+
 # Create Grafana admin credentials secret
 cat <<EOF | kubectl apply -f -
 ---
@@ -14,29 +16,29 @@ apiVersion: v1
 kind: Secret
 metadata:
   name: grafana-admin-credentials
-  namespace: monitoring
+  namespace: observability
 type: Opaque
 data:
   admin-user: $(echo -n "admin" | base64)
-  admin-password: $(echo -n "YOUR_PASSWORD" | base64)
+  admin-password: $(echo -n "n0JKYORwlxl220ZsdkjN1lWABCnvRn0+S7b2cBNccgQ=" | base64)
 EOF
 
 # Install kube-prometheus-stack (includes Prometheus, Grafana, Alertmanager, Node Exporter, etc.)
 # See values file for configuration
-helm upgrade --install monitoring-stack prometheus-community/kube-prometheus-stack \
-  --namespace "monitoring" \
+helm upgrade --install grafana prometheus-community/kube-prometheus-stack \
+  --namespace "observability" \
   --create-namespace \
   --values ./k3s/helm/values/grafana.values.yaml
 
 # Install Loki for logging
 # See values file for configuration
 helm upgrade --install loki grafana/loki \
-  --namespace "monitoring" \
+  --namespace "observability" \
   --values ./k3s/helm/values/loki.values.yaml
 
 # Install Grafana Alloy for log collection (replaced Promtail)
 # It automatically discovers and collects logs from all pods in all namespaces
 # It runs as a DaemonSet and uses Kubernetes service discovery to find pods
 helm upgrade --install alloy grafana/alloy \
-  --namespace "monitoring" \
+  --namespace "observability" \
   --values ./k3s/helm/values/alloy.values.yaml
