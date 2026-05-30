b097cd6c64ef3842251df4e755face2d4b8271af
seemywings <kevin.jayne@icloud.com>
2026-05-30 14:01:23 -0400

    ClusterSecretStore

--- k3s/05-external-secrets.sh
+++ k3s/05-external-secrets.sh
@@ -13,3 +13,46 @@ helm upgrade --install external-secrets external-secrets/external-secrets \
   --create-namespace \
   --set installCRDs=true \
   --values ./k3s/helm/values/external-secrets.values.yaml
+
+
+# Enable GCP Secret Manager API and create GCP SA
+GCLOUD_PROJECT=living-room-cloud
+gcloud --project=${GCLOUD_PROJECT} services enable secretmanager.googleapis.com
+gcloud --project=${GCLOUD_PROJECT} iam service-accounts create external-secrets
+gcloud projects add-iam-policy-binding ${GCLOUD_PROJECT} \
+  --member="serviceAccount:external-secrets@${GCLOUD_PROJECT}.iam.gserviceaccount.com" \
+  --role="roles/secretmanager.secretAccessor"
+
+# Create GCP SA key
+gcloud iam service-accounts keys create gcp-eso-key.json \
+  --iam-account=external-secrets@${GCLOUD_PROJECT}.iam.gserviceaccount.com
+
+# Create Kubernetes secret from GCP SA key
+GCP_SA_KEY_FILE="${GCP_SA_KEY_FILE:-./gcp-eso-key.json}"
+[[ -f "${GCP_SA_KEY_FILE}" ]] || { echo "GCP SA key file not found: ${GCP_SA_KEY_FILE}" >&2; exit 1; }
+
+kubectl create secret generic gcp-secret-manager-credentials \
+  --from-file=secret-access-credentials="${GCP_SA_KEY_FILE}" \
+  -n external-secrets \
+  --dry-run=client -o yaml | kubectl apply -f -
+
+rm ${GCP_SA_KEY_FILE}
+
+# GCP SecretStore
+cat <<EOF | kubectl apply -f -
+---
+apiVersion: external-secrets.io/v1
+kind: ClusterSecretStore
+metadata:
+  name: gcp-secret-manager
+spec:
+  provider:
+    gcpsm:
+      projectID: living-room-cloud
+      auth:
+        secretRef:
+          secretAccessKeySecretRef:
+            name: gcp-secret-manager-credentials
+            key: secret-access-credentials
+            namespace: external-secrets
+EOF
